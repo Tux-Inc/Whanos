0572c98ddb4674f5cdd1f80c30934e1c5e1bc75f
Neeptossss <gwenael.hubler@epitech.eu>
2023-11-23 22:29:21 +0100

    refacto(chore): switch to helm

---
+++ docs/install.md
@@ -0,0 +1,1 @@
+read this : https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -1,37 +1,59 @@
 import java.io.File
+import hudson.plugins.git.GitSCM
+import hudson.plugins.git.extensions.impl.RelativeTargetDirectory
+import hudson.plugins.git.extensions.impl.CloneOption
 
-def imagesDir = new File('/images')
+// Constants
+final String REPO_URL = "https://github.com/Tux-Inc/Whanos.git"
+final String IMAGES_DIR_RELATIVE_PATH = "images"
+final String BASE_FOLDER_NAME = "Whanos base images"
+final String PROJECTS_FOLDER_NAME = "Projects"
+
+// Utility function to clone the repository and return the directory path
+def cloneRepoAndGetImagesDir() {
+    def workspace = new File('/tmp/whanos_repo')
+    def scm = new GitSCM(REPO_URL)
+    scm.extensions.add(new CloneOption(false, false, "", 10))
+    scm.extensions.add(new RelativeTargetDirectory(workspace.getAbsolutePath()))
+
+    // Clone the repo
+    scm.checkout(null, workspace, null, null, null, null)
+
+    // Return path to images directory
+    return new File(workspace, IMAGES_DIR_RELATIVE_PATH)
+}
+
+def imagesDir = cloneRepoAndGetImagesDir()
 def languages = []
 
 if (imagesDir.exists() && imagesDir.isDirectory()) {
-    File[] files = imagesDir.listFiles()
-    def directories = files.findAll { it.isDirectory() }
+    def directories = imagesDir.listFiles().findAll { it.isDirectory() }
     languages = directories.collect { it.name.capitalize() }
     println "Available languages: " + languages
 } else {
     println "Images directory not found or is not a directory, no languages available"
 }
 
-folder("Whanos base images") {
+folder(BASE_FOLDER_NAME) {
     description("Whanos base images folder")
 }
 
-folder("Projets") {
-    description("Projets folder")
+folder(PROJECTS_FOLDER_NAME) {
+    description("Projects folder")
 }
 
 languages.each { language ->
-    freeStyleJob("Whanos base images/whanos-$language") {
+    freeStyleJob("$BASE_FOLDER_NAME/whanos-$language") {
         steps {
-            shell("docker build /images/$language -t whanos-$language -f /images/$language/Dockerfile.base")
+            shell("docker build ${imagesDir.absolutePath}/$language -t whanos-$language -f ${imagesDir.absolutePath}/$language/Dockerfile.base")
         }
     }
 }
 
-freeStyleJob("Whanos base images/Build all base images") {
+freeStyleJob("$BASE_FOLDER_NAME/Build all base images") {
     publishers {
         downstreamParameterized {
-            trigger("Whanos base images/whanos-$language", false)
+            trigger("$BASE_FOLDER_NAME/whanos-$language", false)
             condition("SUCCESS")
             parameters {
                 predefinedProp("language", languages.join(','))
@@ -62,7 +84,6 @@ freeStyleJob("link-project") {
                         shell("echo 'TODO: BUILD IMAGE'")
                     }
                 }
-
             ''')
         }
     }
