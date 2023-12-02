fd72dd88d0e4ca833582c581a94e53aafc12cb1b
Neeptossss <gwenael.hubler@epitech.eu>
2023-12-02 18:11:18 +0100

    ci(vagrant): init

--- scripts/installation/jenkins.sh
+++ scripts/installation/jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 printf "Installing jenkins"
-helm install whanos /whanos/kube/helm/whanos --create-namespace --namespace whanos
+helm install whanos kube/helm/whanos --create-namespace --namespace whanos
 printf "Installed jenkins ✅"
